1c1486426a1b038fc9150efa03f63b6e75630520
darikmohammed <r.m.darik@gmail.com>
2022-12-21 10:18:30 +0300

    Add comment resource

--- app/components/posts/post_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Posts::PostComponent < ViewComponent::Base
-  def initialize(post:)
-    @post = post
-  end
-
-end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates :content, presence: true
 
   has_rich_text :content
+  has_many :comments
 end
